797a96863b983b97c0da96863b7d494e3d603c5b
lq08025107 <534525962@qq.com>
2017-02-08 20:08:35 +0800

    add some function

--- src/main/java/net/iliuqiang/webapi/APIController.java
+++ src/main/java/net/iliuqiang/webapi/APIController.java
@@ -1,6 +1,9 @@
 package net.iliuqiang.webapi;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLNonTransientConnectionException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -14,6 +17,9 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,7 +30,11 @@ public class APIController {
 	private final static String baseUrl = "http://cn.bing.com";
 	private final static String bingUrl = "http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1"; 
 	private String picUrl= null;
-	
+	public static String xmlContent;
+	static{
+		xmlContent = getFromFile();
+		System.out.println(xmlContent);
+	}
 	@RequestMapping(value = "/bing_pic", produces = "text/plain ; charset=utf-8")
 	@ResponseBody
 	public String getBingPic(){
@@ -38,6 +48,7 @@ public class APIController {
 			HttpEntity entity = response.getEntity();
 			if(entity != null){
 				content = EntityUtils.toString(entity, "UTF-8");
+				System.out.println(content);
 			}
 			try {
 				Document document = DocumentHelper.parseText(content);
@@ -67,4 +78,52 @@ public class APIController {
 		return picUrl;
 		
 	}
+	@RequestMapping(value = "/china", produces = "application/json ; charset=utf-8")
+	@ResponseBody
+	public void getProvince(){
+		try {
+			Document document = DocumentHelper.parseText(xmlContent);
+			Element root = document.getRootElement();
+			Element province = root.element("province");
+			System.out.println(province.getText());
+			
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("--------------province call-----------");
+	}
+	@RequestMapping(value = "/china/{province}", produces = "application/json ; charset=utf-8")
+	@ResponseBody
+	public void getCity(){
+		System.out.println("--------------city call-----------");
+	}
+	@RequestMapping(value = "/china/{province}/{city}", produces = "application/json ; charset=utf-8")
+	@ResponseBody
+	public void getCounty(){
+		System.out.println("--------------county call-----------");
+	}
+	
+	private static String  getFromFile(){
+		
+		ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+		// The path to the resource from the root of the JAR file
+		Resource file = resourceResolver.getResource("classpath:/china_city_list.xml");
+		BufferedReader reader = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+			String temp;
+			while ((temp = reader.readLine()) != null) {
+				sb.append(temp);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String xmlContent = sb.toString();
+		return xmlContent;
+	}
+
 }
